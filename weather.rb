d9c1da280aeb85eb36ace1d027e3cd5e85a61f13
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-09 20:11:17 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -42,7 +42,7 @@ weather = {
 	:temperature_farenheit => 66.3
 }
 
-puts "The weather in #{weather[:city]} is #{weather[:description]}. The temperature is #{weather[:temperature_farenheit]} "
+return "The weather in #{weather[:city]} is #{weather[:description]}. The temperature is #{weather[:temperature_farenheit]} "
 end
 current_weather
 
