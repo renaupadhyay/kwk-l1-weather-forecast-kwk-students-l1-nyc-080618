321bad6e38044a6640de204d8fab1dc0f4a5d6b3
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-09 19:39:04 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -7,28 +7,39 @@
 # }
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
-
+def location
   weather= {}
   weather["New York"]= 84
   
   puts weather
+  
+end
+location
+
+
   
 	
 
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
-description = { "New York"=> "Partly Cloudy", "Chicago"=> "Cloudy"}
-
-puts description["Chicago"]
+def description
+  weather= {}
+  weather["New York"]= "sunny"
+  
+  puts weather
+  
+end
+description
+  
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
 weather_hash= {}
 counter=0 
- location= ["New York", "Chicago"]
- description= ["Partly Cloudy", "Cloudy"]
- temperature= [84,82]
+ location= ["New York"]
+ description= ["Partly Cloudy"]
+ temperature= [84]
 
 weather_hash
 puts "The weather in "
